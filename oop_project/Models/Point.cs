934bcb2f71eda96af45a5677e47332167ba43ecf
Andrew <andrej.gorokhoff2017@yandex.ru>
2021-09-28 21:21:01 +0300

    Add Class Point

---
+++ oop_project/Models/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_project.Models
+{
+    public class Point
+    {
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+    }
+}
